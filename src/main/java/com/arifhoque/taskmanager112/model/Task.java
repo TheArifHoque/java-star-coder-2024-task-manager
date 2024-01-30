e27fc8c90f0bfd79fd5c18ecbbf352f49d57d4b5
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-01-30 23:06:46 +0600

    task model updated

---
+++ src/main/java/com/arifhoque/taskmanager112/model/Task.java
@@ -0,0 +1,27 @@
+package com.arifhoque.taskmanager112.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "task_table")
+public class Task {
+    @Id
+    @GeneratedValue
+    @Column(name = "task_id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "status")
+    private String status;
+}
