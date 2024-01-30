893ab8cc2c3d5cec49e6dbd9f5df2fc80cccfcbc
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-01-30 23:16:00 +0600

    authority model updated

---
+++ src/main/java/com/arifhoque/taskmanager112/model/Authority.java
@@ -0,0 +1,21 @@
+package com.arifhoque.taskmanager112.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "authority_table")
+public class Authority {
+    @Id
+    @GeneratedValue
+    @Column(name = "authority_id")
+    private Integer id;
+
+    @Column(name = "authority", unique = true)
+    private String authority;
+}
