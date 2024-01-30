e03364bf5e6ab4b160f2fc69fd42e9cfda2f7904
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-01-30 22:44:03 +0600

    initial commit

---
+++ src/main/java/com/arifhoque/taskmanager112/TaskManager112Application.java
@@ -0,0 +1,13 @@
+package com.arifhoque.taskmanager112;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManager112Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManager112Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/arifhoque/taskmanager112/TaskManager112ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arifhoque.taskmanager112;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManager112ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
